ab4635c9a2683a0778a4a22df9e031f2d6828e58
DiluTharushika <165666320+DiluTharushika@users.noreply.github.com>
2026-05-21 11:55:39 +0530

    wrap application with ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
     <AuthProvider>
       {/* BrowserRouter enables route-based pages */}
       <BrowserRouter>
+        <ThemeProvider>
         <App />
+      </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>
